2f6621f55fb84d8ff38256eaca0b124bda37d90a
Hemanth <hemu161@gmail.com>
2018-07-04 18:27:18 +0530

    login

--- express_server/index.js
+++ express_server/index.js
@@ -10,6 +10,13 @@ const metaAuth = new MetaAuth({banner:"this is to verify your ethereum address"}
 const server = http.createServer(app);
 const io = socketIo(server);
 
+const jwt = require('jsonwebtoken');
+const exjwt = require('express-jwt');
+
+// INstantiating the express-jwt middleware
+const jwtMW = exjwt({
+    secret: 'keyboard cat 4 ever'
+});
 
 app.get('/auth/:MetaAddress', metaAuth, (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -25,16 +32,25 @@ app.get('/auth/:MetaMessage/:MetaSignature', metaAuth, (req, res) => {
   if (req.metaAuth && req.metaAuth.recovered) {
     // Signature matches the cache address/challenge
     // Authentication is valid, assign JWT, etc.
-    res.send(req.metaAuth.recovered);
+    let token = jwt.sign({ username: req.metaAuth.recovered }, 'keyboard cat 4 ever', { expiresIn: 129600 }); // Sigining the token
+    // res.send(req.metaAuth.recovered);
+    res.json({ sucess: true,err: null,token});
+
   } else {
     // Sig did not match, invalid authentication
     res.status(400).send();
   };
 });
 
-app.get('/',(req, res) => {
-  res.send({ response: "I am alive" }).status(200);
-});	
+app.get('/', jwtMW /* Using the express jwt MW here */, (req, res) => {
+    console.log('getting request');
+    // res.send('You are authenticated'); //Sending some response when authenticated
+    res.json({
+                sucess: true,
+                err: null,
+                message:'you are authenticated'
+            });
+});
 
 io.on("connection", socket => {
   console.log("New client connected");
